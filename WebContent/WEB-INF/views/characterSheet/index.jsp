f187d56eecbe97c073ee88a29dbfaf903eba95e7
akane-senseki <ajajggpw@gmail.com>
2020-12-22 23:47:14 +0900

    2020.12.22

--- WebContent/WEB-INF/views/characterSheet/index.jsp
+++ WebContent/WEB-INF/views/characterSheet/index.jsp
@@ -1,7 +1,11 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <c:import url="../layout/app.jsp">
     <c:param name="content">
         <h2>キャラシ一覧ページ！の予定</h2>
+        <p>
+            <a href="<c:url value="/charactersheet/new"/>">新規キャラシートの登録</a>
+        </p>
     </c:param>
 </c:import>
--- src/controllers/character_sheet/CharacterSheetNewServlet.java
+++ src/controllers/character_sheet/CharacterSheetNewServlet.java
@@ -1,19 +1,23 @@
 package controllers.character_sheet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Pc_Entity;
+
 /**
  * Servlet implementation class CharacterSheetNewServlet
  */
 @WebServlet("/charactersheet/new")
 public class CharacterSheetNewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +26,16 @@ public class CharacterSheetNewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("employee", new Pc_Entity());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/characterSheet/new.jsp");
+        rd.forward(request, response);
+
+    }
 
 }
